b85302d0e80cca061d58d425113f182ca401e020
Juhyun Na <ghghtygh@naver.com>
2022-10-26 18:54:08 +0900

    [level 3] Title: 베스트앨범, Time: 2.69 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -1,50 +1,60 @@
 
 import java.util.*;
-import static java.util.Collections.sort;
 
 class Solution {
-public int[] solution(String[] genres, int[] plays) {
+    static class Song {
+		int idx;
+		String genre;
+		int play;
 
-        Map<String, Integer> map1 = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) {
-            map1.put(genres[i], map1.getOrDefault(genres[i], 0) + plays[i]);
-        }
-        Map<Integer, String> map2 = new TreeMap<>(Comparator.reverseOrder());
-        for (Map.Entry<String, Integer> entry : map1.entrySet()) {
-            map2.put(entry.getValue(), entry.getKey());
-        }
+		int genreIdx = 1;
 
+		public Song(int idx, String genre, int play) {
+			this.idx = idx;
+			this.genre = genre;
+			this.play = play;
+		}
+	}
+    public int[] solution(String[] genres, int[] plays) {
 
-        List<Integer> resultList = new ArrayList<>();
-        for (Map.Entry<Integer, String> entry : map2.entrySet()) {
-            List<Map> list = new ArrayList<>();
-//            System.out.println("entry.getValue() = " + entry.getValue());
-            for (int i = 0; i < genres.length; i++) {
-                if (genres[i].equals(entry.getValue())) {
-                    Map<String, Integer> map = new HashMap<>();
-                    map.put("index", i);
-                    map.put("count", plays[i]);
-                    list.add(map);
-                }
-            }
-            sort(list, new Comparator<Map>() {
-                @Override
-                public int compare(Map o1, Map o2) {
-                    return (int) o2.get("count") - (int) o1.get("count");
-                }
-            });
+		Map<String, Integer> genreMap = new HashMap<>();
+		List<Song> songList = new ArrayList<>();
 
-            resultList.add((int) list.get(0).get("index"));
-            if(list.size() > 1){
-                resultList.add((int) list.get(1).get("index"));
-            }
-        }
+		for (int i = 0; i < genres.length; i++) {
+			genreMap.put(genres[i], genreMap.getOrDefault(genres[i], 0) + plays[i]);
+			songList.add(new Song(i, genres[i], plays[i]));
+		}
 
-        int[] result = new int[resultList.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = resultList.get(i);
-//            System.out.println("resultList.get(i) = " + resultList.get(i));
-        }
-        return result;
-    }
+		Collections.sort(songList, (o1, o2) -> {
+
+			int compareGenre = genreMap.get(o2.genre) - genreMap.get(o1.genre);
+			if (compareGenre != 0) {
+				return compareGenre;
+			}
+
+			int comparePlay = o2.play - o1.play;
+			if (comparePlay != 0) {
+				return comparePlay;
+			}
+
+			return o1.idx - o2.idx;
+
+		});
+
+		for (int i = 1; i < songList.size(); i++) {
+			Song now = songList.get(i);
+			Song prev = songList.get(i - 1);
+			
+            if (now.genre.equals(prev.genre)) {
+				now.genreIdx = prev.genreIdx + 1;
+			}
+            
+			if (now.genreIdx > 2) {
+				songList.remove(now);
+			}
+		}
+		return songList.stream()
+			.mapToInt(song -> song.idx)
+			.toArray();
+	}
 }
