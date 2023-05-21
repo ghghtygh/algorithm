293ebc5e23285de8f05ca6c7ebbc5eea78c67f52
jupo13 <ghghtygh@naver.com>
2023-05-21 16:42:41 +0900

    [unrated] Title: 달리기 경주, Time: 0.05 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -4,27 +4,23 @@ import java.util.Map;
 class Solution {
     
     public static String[] solution(String[] players, String[] callings) {
-        String[] answer = new String[players.length];
+        String[] answer = players;
         Map<String, Integer> playersMap = new HashMap<>();
-        Map<Integer, String> scoreMap = new HashMap<>();
+
         for(int i = 0; i<players.length;i++){
             playersMap.put(players[i], i);
-            scoreMap.put(i, players[i]);
+
         }
 
         for(String calling : callings) {
             int idx = playersMap.get(calling);
-            String player = scoreMap.get(idx - 1);
+            String player = answer[idx - 1];
 
             playersMap.put(player, idx);
             playersMap.put(calling, idx - 1);
 
-            scoreMap.put(idx, player);
-            scoreMap.put(idx - 1, calling);
-        }
-
-        for(int i = 0; i < players.length; i++) {
-            answer[i] = scoreMap.get(i);
+            answer[idx] = player;
+            answer[idx-1] = calling;
         }
 
         return answer;
